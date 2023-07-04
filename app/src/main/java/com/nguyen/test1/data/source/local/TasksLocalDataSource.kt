3576094b53e3acf0e4c0453f09a566daff42ad46
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-07-04 14:56:40 -0700

    b1: WIP

---
+++ app/src/main/java/com/nguyen/test1/data/source/local/TasksLocalDataSource.kt
@@ -0,0 +1,92 @@
+package com.nguyen.test1.data.source.local
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.nguyen.test1.data.Result
+import com.nguyen.test1.data.Task
+import com.nguyen.test1.data.source.TasksDataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Concrete implementation of a data source as a db.
+ */
+class TasksLocalDataSource internal constructor(
+    private val tasksDao: TasksDao,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : TasksDataSource {
+
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        return tasksDao.observeTasks().map {
+            Result.Success(it)
+        }
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        return tasksDao.observeTaskById(taskId).map {
+            Result.Success(it)
+        }
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        //NO-OP
+    }
+
+    override suspend fun refreshTasks() {
+        //NO-OP
+    }
+
+    override suspend fun getTasks(): Result<List<Task>> = withContext(ioDispatcher) {
+        return@withContext try {
+            Result.Success(tasksDao.getTasks())
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun getTask(taskId: String): Result<Task> = withContext(ioDispatcher) {
+        try {
+            val task = tasksDao.getTaskById(taskId)
+            if (task != null) {
+                return@withContext Result.Success(task)
+            } else {
+                return@withContext Result.Error(Exception("Task not found!"))
+            }
+        } catch (e: Exception) {
+            return@withContext Result.Error(e)
+        }
+    }
+
+    override suspend fun saveTask(task: Task) = withContext(ioDispatcher) {
+        tasksDao.insertTask(task)
+    }
+
+    override suspend fun completeTask(task: Task) = withContext(ioDispatcher) {
+        tasksDao.updateCompleted(task.id, true)
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        tasksDao.updateCompleted(taskId, true)
+    }
+
+    override suspend fun activateTask(task: Task) = withContext(ioDispatcher) {
+        tasksDao.updateCompleted(task.id, false)
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        tasksDao.updateCompleted(taskId, false)
+    }
+
+    override suspend fun clearCompletedTasks() = withContext<Unit>(ioDispatcher) {
+        tasksDao.deleteCompletedTasks()
+    }
+
+    override suspend fun deleteAllTasks() = withContext(ioDispatcher) {
+        tasksDao.deleteTasks()
+    }
+
+    override suspend fun deleteTask(taskId: String) = withContext<Unit>(ioDispatcher) {
+        tasksDao.deleteTaskById(taskId)
+    }
+}
---
+++ app/src/main/java/com/nguyen/test1/statistics/StatisticsFragment.kt
@@ -0,0 +1,39 @@
+package com.nguyen.test1.statistics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.nguyen.test1.R
+import com.nguyen.test1.databinding.StatisticsFragBinding
+import com.nguyen.test1.util.setupRefreshLayout
+
+/**
+ * Main UI for the statistics screen.
+ */
+class StatisticsFragment : Fragment() {
+
+    private lateinit var viewDataBinding: StatisticsFragBinding
+
+    private val viewModel by viewModels<StatisticsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = DataBindingUtil.inflate(
+            inflater, R.layout.statistics_frag, container,
+            false
+        )
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewDataBinding.viewmodel = viewModel
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+    }
+}
